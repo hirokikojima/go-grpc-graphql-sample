56bc0b80973e2e729292a46de5c847b028413e46
h_kojima <h_kojima@smiloops.com>
2020-12-16 14:14:14 +0900

    first commit

---
+++ graph/resolver.go
@@ -0,0 +1,13 @@
+package graph
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+import (
+	"github.com/hirokikojima/go-sample-graphql-grpc/graph/model"
+)
+
+type Resolver struct{
+	todos []*model.Todo
+}
